4c58bd8ee4a4ebd28550fa126f6db45e21a183f1
Matthew Martelle <matthew.martelle@aofl.com>
2019-02-28 00:00:25 -0800

    it's working it's working, now for styles

--- server/common/sockets.js
+++ server/common/sockets.js
@@ -6,6 +6,7 @@ const rooms = {};
 const generateRoomName = creatorId => {
   let roomName = null;
   const randExp = new RandExp(/^[A-Z]{5}/);
+  // avoiding any room names that already exist
   while (typeof roomName !== 'string' || rooms[roomName]) {
     roomName = randExp.gen();
   }
@@ -16,32 +17,38 @@ const generateRoomName = creatorId => {
 const sockets = server => {
   const io = socketio(server);
   io.on('connection', socket => {
+    function joinRoom(roomName, isCreator = false) {
+      if (rooms[roomName]) {
+        socket.join(roomName, () => {
+          console.log(socket.id, ' joined room: ', roomName);
+          socket.emit('roomJoined', { roomName, isCreator, creatorId: rooms[roomName].creatorId, socketId: socket.id});
+          socket.on('idea update', (idea, isNewIdea) => {
+            console.log('idea: ', idea);
+            socket.in(roomName).emit('idea update', idea, isNewIdea);
+          });
+          // Getting full state from the brainstorm creator
+          if (isCreator) {
+            socket.on('brainstorm state send', (requesterId, brainstormState) => {
+              io.to(`${requesterId}`).emit('brainstorm state sent', brainstormState);
+            });
+          } else {
+            socket.on('brainstorm state request', creatorId => {
+              io.to(`${creatorId}`).emit('brainstorm state requested', socket.id);
+            });
+          }
+        });
+      } else {
+        console.log('Error: The requested brainstorm does not exist');
+        socket.emit('room not found', 'The requested brainstorm does not exist');
+      }
+    }
+    socket.emit('connected', socket.id);
     console.log(`New connection from ${socket.handshake.address}, id:${socket.id}`);
     socket.on('createRoom', () => {
       const roomName = generateRoomName(socket.id);
-      socket.join(roomName, () => {
-        socket.emit('roomCreated', { roomName, socketId: socket.id });
-        console.log('room created: ', roomName);
-        socket.on('idea update', idea => {
-          socket.in(roomName).emit('idea update', idea);
-        });
-        // socket.on(`get ${roomName} data`);
-      });
+      joinRoom(roomName, true);
     });
-    socket.on('join room', roomName => {
-      if (rooms[roomName]) {
-        socket.join(roomName, () => {
-          console.log('room joined: ', roomName);
-          socket.emit('roomJoined', { roomName, socketId: socket.id });
-          socket.on('idea update', idea => {
-            socket.in(roomName).emit('idea update', idea);
-          });
-        });
-      } else {
-        console.log('error', 'The requested brainstorm does not exist');
-        socket.emit('error', 'The requested brainstorm does not exist');
-      }
-    });
+    socket.on('join room', joinRoom);
   });
 };
 
